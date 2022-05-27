e1d5ada6c577a5cbbabeb8a4dc9774558a8d8999
kmap-holdings <kmaphane@gmail.com>
2022-05-27 13:04:41 +0200

    another one

--- public/js/app.js
+++ public/js/app.js
@@ -157,22 +157,4 @@
             },
         },
     });
-
-    // ================ pricing tab
-    const tabs = document.querySelectorAll("[data-tab-target]");
-    const tabContents = document.querySelectorAll("[data-tab-content]");
-
-    tabs.forEach((tab) => {
-        tab.addEventListener("click", () => {
-            const target = document.querySelector(tab.dataset.tabTarget);
-            tabContents.forEach((tabContent) => {
-                tabContent.classList.remove("active");
-            });
-            tabs.forEach((tab) => {
-                tab.classList.remove("active");
-            });
-            tab.classList.add("active");
-            target.classList.add("active");
-        });
-    });
 })();
